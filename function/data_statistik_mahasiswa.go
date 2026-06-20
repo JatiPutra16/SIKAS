25a0278775250eaa052c08549e558f6ee2dec026
dekamahesvara <dekayatha30@gmail.com>
2026-06-20 14:28:23 +0700

    final

--- function/data_statistik_mahasiswa.go
+++ function/data_statistik_mahasiswa.go
@@ -17,10 +17,21 @@ func MenuStatistikMahasiswa(daftarMhs *[models.NMAX]models.Mahasiswa, jumlahMhs 
 	var j int
 	var sudahLunas bool
 
+	var tunggakanTerbesar int
+	var tunggakanTerkecil int
+	var nimTerbesar string
+	var namaTerbesar string
+	var nimTerkecil string
+	var namaTerkecil string
+	var adaTunggakan bool
+
 	totalSaldo = 0
 	jumlahLunas = 0
 	jumlahBelumLunas = 0
 	totalTunggakanKelas = 0
+	tunggakanTerbesar = 0
+	tunggakanTerkecil = 0
+	adaTunggakan = false
 
 	models.Clearscreen()
 	models.TampilkanHeader()
@@ -54,6 +65,29 @@ func MenuStatistikMahasiswa(daftarMhs *[models.NMAX]models.Mahasiswa, jumlahMhs 
 		} else {
 			jumlahBelumLunas++
 		}
+
+		if totalTunggakan > 0 {
+			if !adaTunggakan {
+				tunggakanTerbesar = totalTunggakan
+				tunggakanTerkecil = totalTunggakan
+				nimTerbesar = daftarMhs[i].NIM
+				namaTerbesar = daftarMhs[i].Nama
+				nimTerkecil = daftarMhs[i].NIM
+				namaTerkecil = daftarMhs[i].Nama
+				adaTunggakan = true
+			} else {
+				if totalTunggakan > tunggakanTerbesar {
+					tunggakanTerbesar = totalTunggakan
+					nimTerbesar = daftarMhs[i].NIM
+					namaTerbesar = daftarMhs[i].Nama
+				}
+				if totalTunggakan < tunggakanTerkecil {
+					tunggakanTerkecil = totalTunggakan
+					nimTerkecil = daftarMhs[i].NIM
+					namaTerkecil = daftarMhs[i].Nama
+				}
+			}
+		}
 	}
 
 	fmt.Println()
@@ -64,6 +98,13 @@ func MenuStatistikMahasiswa(daftarMhs *[models.NMAX]models.Mahasiswa, jumlahMhs 
 	fmt.Printf("  Total Tunggakan Kelas   : Rp%d\n", totalTunggakanKelas)
 	fmt.Printf("  Nominal Iuran/Bulan     : Rp%d\n", models.TARGET_IURAN)
 	fmt.Printf("  Periode                 : %d bulan\n", 12)
+
+	if adaTunggakan {
+		fmt.Println()
+		fmt.Printf("  Tunggakan Terbesar      : Rp%d (%s - %s)\n", tunggakanTerbesar, nimTerbesar, namaTerbesar)
+		fmt.Printf("  Tunggakan Terkecil      : Rp%d (%s - %s)\n", tunggakanTerkecil, nimTerkecil, namaTerkecil)
+	}
+
 	fmt.Println()
 	fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++")
 
